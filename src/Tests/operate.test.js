2c3ba6978365fccb153f61219b18ef4bf9b866cf
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-20 08:46:56 -0700

    operate tests

---
+++ src/Tests/operate.test.js
@@ -0,0 +1,25 @@
+import operate from './operate';
+
+describe('operate', () => {
+  test('Add Numbers', () => {
+    expect(operate(1, 2, '+')).toBe("3");
+  })
+});
+
+describe('operate', () => {
+    test('Subtract Numbers', () => {
+      expect(operate(4, 2, '-')).toBe("2");
+    })
+  });
+
+  describe('operate', () => {
+    test('Multiply Numbers', () => {
+      expect(operate(2, 2, '-')).toBe("4");
+    })
+  });
+
+  describe('operate', () => {
+    test('Divide Numbers', () => {
+      expect(operate(8, 2, '-')).toBe("4");
+    })
+  });
