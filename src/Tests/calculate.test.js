c3bbb0fc41a28837e946945f79c1d5baec4381d9
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-20 11:23:26 -0700

    Fraction Test Added

--- src/Tests/calculate.test.js
+++ src/Tests/calculate.test.js
@@ -7,3 +7,10 @@ describe('calculate', () => {
     expect(calculate(calculator, 'AC')).toEqual({ total: null, next: null, operation: null });
   });
 });
+
+describe('calculate', () => {
+  test('fraction decimal', () => {
+    const calculator = { total: '0', next: '5.', operation: '.' };
+    expect(calculate(calculator, '.')).toEqual({ total: '0', next: '5.', operation: '.' });
+  });
+});
