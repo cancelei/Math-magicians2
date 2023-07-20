2b5bd33b3ce5220beaca6218ae8be7da0399bcac
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-20 10:04:47 -0700

    Test Passed

--- src/Tests/operate.test.js
+++ src/Tests/operate.test.js
@@ -1,25 +1,32 @@
-import operate from './operate';
+// eslint-disable-next-line import/extensions
+import operate from '../logic/operate';
 
 describe('operate', () => {
   test('Add Numbers', () => {
-    expect(operate(1, 2, '+')).toBe("3");
-  })
+    expect(operate(1, 2, '+')).toBe('3');
+  });
 });
 
 describe('operate', () => {
-    test('Subtract Numbers', () => {
-      expect(operate(4, 2, '-')).toBe("2");
-    })
+  test('Subtract Numbers', () => {
+    expect(operate(4, 2, '-')).toBe('2');
   });
+});
 
-  describe('operate', () => {
-    test('Multiply Numbers', () => {
-      expect(operate(2, 2, '-')).toBe("4");
-    })
+describe('operate', () => {
+  test('Multiply Numbers', () => {
+    expect(operate(2, 2, 'x')).toBe('4');
   });
+});
 
-  describe('operate', () => {
-    test('Divide Numbers', () => {
-      expect(operate(8, 2, '-')).toBe("4");
-    })
+describe('operate', () => {
+  test('Divide Numbers', () => {
+    expect(operate(8, 2, '÷')).toBe('4');
   });
+});
+
+describe('operate', () => {
+  test('Divide Numbers', () => {
+    expect(operate(8, 2, '%')).toBe('0');
+  });
+});
